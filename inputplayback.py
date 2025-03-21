118157e14b58d457636df92eb41069a7e7f5f42b
Mouro <mourossonero@gmail.com>
2025-03-21 00:43:33 -0400

    Added the functionality to restart & record new sequence or exit program

--- inputplayback.py
+++ inputplayback.py
@@ -3,24 +3,38 @@ import keyboard
 #Required to monitor 'stop key' press during playback
 import threading
 
-# Variable to control the loop
+# Variables to control the loop
 stop_playback = False
+# Function to enable multiple recordings
+def replay_requested():
+    global stop_playback
+    print("Press 'r' to restart or 'ctrl' to exit\n")
+    while True:
+        if keyboard.is_pressed('r'):
+            stop_playback = False
+            input_playback()
+        elif keyboard.is_pressed('ctrl'):
+            break
+
 # Function to make CTRL 'stop key' when the next replay ends
 def check_for_stop_key():
     global stop_playback
     while not stop_playback:
         if keyboard.is_pressed('ctrl'):
-            print("CTRL has been pressed, ending program")
+            print("\n'ctrl' has been pressed, ending replay")
             stop_playback = True
             break
 
 # Main function of the program, recording and playing back sequence
 def input_playback():
     try:
-        print("Input the desired sequence now then press Shift to stop recording")
+        print("Press 'shift' to start/stop recording")
+        keyboard.wait('shift')
+        print("\nRecording now...")
         # Records the inputs pressed, held, released until Shift is pressed
         events = keyboard.record('shift')
-        print("Press CTRL to stop the program")
+        print("Sequence has been recorded\n")
+        print("Press 'ctrl' to stop the program\n")
         # Start a thread to watch the 'stop key'
         stop_thread = threading.Thread(target=check_for_stop_key, daemon=True)
         stop_thread.start()
@@ -29,8 +43,12 @@ def input_playback():
             print("Now replaying the input sequence...")
             # Playback the recording at normal speed
             keyboard.play(events, speed_factor=1.0)
+        replay_requested()
+
     except KeyboardInterrupt:
         pass
 
 #Run the function
+print("Welcome to the Budokai Trainer tool!\n")
 input_playback()
+print("Ending Program...\n")
